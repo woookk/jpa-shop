585ef90de00fa37643af3c0b418ead499a9e3b1f
woookk <woook27@naver.com>
2024-04-03 13:01:49 +0900

    Feat: 엔티티 설계시 주의점 반영

--- src/main/java/com/woookk/jpabook/jpashop/domain/Category.java
+++ src/main/java/com/woookk/jpabook/jpashop/domain/Category.java
@@ -20,16 +20,21 @@ public class Category {
 
     private String name;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "category_item",
             joinColumns = @JoinColumn(name = "category_id"),
             inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
